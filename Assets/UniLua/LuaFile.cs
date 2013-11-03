324a94b0939f6aeee1bbfa7215afee27eb370a07
arnan <shenglunan@gmail.com>
2013-11-03 16:19:00 +0800

    完善 LuaFile 中对路径的处理

--- Assets/UniLua/LuaFile.cs
+++ Assets/UniLua/LuaFile.cs
@@ -8,17 +8,17 @@ namespace UniLua
 {
 	internal class LuaFile
 	{
-		private static readonly string LUA_ROOT = Application.dataPath + "/StreamingAssets/LuaRoot/";
+		private static readonly string LUA_ROOT = System.IO.Path.Combine(Application.streamingAssetsPath, "LuaRoot");
 
 		public static FileLoadInfo OpenFile( string filename )
 		{
-			var path = LUA_ROOT + filename;
+			var path = System.IO.Path.Combine(LUA_ROOT, filename);
 			return new FileLoadInfo( File.Open( path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite ) );
 		}
 
 		public static bool Readable( string filename )
 		{
-			var path = LUA_ROOT + filename;
+			var path = System.IO.Path.Combine(LUA_ROOT, filename);
 			try {
 				using( var stream = File.Open( path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite ) ) {
 					return true;
