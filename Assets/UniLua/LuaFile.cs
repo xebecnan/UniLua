60be7029bfab8964f754f5fa9a93682ee032f58b
fum1h1ro <fumihiro@gmail.com>
2015-01-23 16:30:18 +0900

    add: hook for reading file path

--- Assets/UniLua/LuaFile.cs
+++ Assets/UniLua/LuaFile.cs
@@ -6,22 +6,26 @@ using UnityEngine;
 
 namespace UniLua
 {
+	public delegate string PathHook(string filename);
 	public class LuaFile
 	{
-		private static string LUA_ROOT = System.IO.Path.Combine(Application.streamingAssetsPath, "LuaRoot");
-    public void SetLuaRoot(string path) {
-      LUA_ROOT = System.IO.Path.Combine(path, "LuaRoot");
-    }
+		//private static readonly string LUA_ROOT = System.IO.Path.Combine(Application.streamingAssetsPath, "LuaRoot");
+		private static PathHook pathhook = (s) => Path.Combine(Path.Combine(Application.streamingAssetsPath, "LuaRoot"), s);
+		public static void SetPathHook(PathHook hook) {
+			pathhook = hook;
+		}
 
 		public static FileLoadInfo OpenFile( string filename )
 		{
-			var path = System.IO.Path.Combine(LUA_ROOT, filename);
+			//var path = System.IO.Path.Combine(LUA_ROOT, filename);
+			var path = pathhook(filename);
 			return new FileLoadInfo( File.Open( path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite ) );
 		}
 
 		public static bool Readable( string filename )
 		{
-			var path = System.IO.Path.Combine(LUA_ROOT, filename);
+			//var path = System.IO.Path.Combine(LUA_ROOT, filename);
+			var path = pathhook(filename);
 			try {
 				using( var stream = File.Open( path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite ) ) {
 					return true;
