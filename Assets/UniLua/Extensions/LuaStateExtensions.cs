4f313f50f2f77ba1b941f2bd9b41f66fdf28991c
TrueNoob141 <truenoob141@gmail.com>
2019-09-09 02:50:10 +0300

    LuaStateExtension: renamed to LuaStateExtensions

--- Assets/UniLua/Extensions/LuaStateExtensions.cs
+++ Assets/UniLua/Extensions/LuaStateExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using UniLua;
 using UnityEngine;
 
-public static class LuaStateExtension {
-  public static Vector3 GetVector(this LuaState lua) {
+public static class LuaStateExtensions
+{
+  public static Vector3 GetVector(this LuaState lua)
+  {
     var tbl = lua.Last().V.HValue();
     var x = tbl.GetFloat("x");
     var y = tbl.GetFloat("y");
@@ -11,12 +13,14 @@ public static class LuaStateExtension {
     return new Vector3(x, y, z);
   }
 
-  public static StkId Last(this LuaState lua) {
+  public static StkId Last(this LuaState lua)
+  {
     var top = lua.Top.Index - 1;
     return lua.Stack[top];
   }
 
-  public static void PushFunctionFromTable(this LuaState lua, LuaTable tbl, string funcName) {
+  public static void PushFunctionFromTable(this LuaState lua, LuaTable tbl, string funcName)
+  {
     var idx = tbl.GetFunction(funcName);
     idx.Index = lua.Top.Index;
     idx.SetList(lua.Stack);
@@ -24,13 +28,15 @@ public static class LuaStateExtension {
     lua.ApiIncrTop();
   }
 
-  public static void PushTable(this LuaState lua, LuaTable tbl) {
+  public static void PushTable(this LuaState lua, LuaTable tbl)
+  {
     lua.Top.V.SetHValue(tbl);
     lua.Top.SetList(lua.Stack);
     lua.ApiIncrTop();
   }
 
-  public static LuaTable ToTable(this LuaState lua, int index, out bool isTable) {
+  public static LuaTable ToTable(this LuaState lua, int index, out bool isTable)
+  {
     StkId addr;
     if (!lua.Index2Addr(index, out addr))
     {
@@ -48,12 +54,14 @@ public static class LuaStateExtension {
     return null;
   }
 
-  public static LuaTable ToTable(this LuaState lua, int index) {
+  public static LuaTable ToTable(this LuaState lua, int index)
+  {
     bool flag = false;
     return lua.ToTable(index, out flag);
   }
 
-  public static void CallProcedure(this LuaState lua, LuaTable table, string name, object[] parameters) {
+  public static void CallProcedure(this LuaState lua, LuaTable table, string name, object[] parameters)
+  {
     lua.PushFunctionFromTable(table, name);
     lua.PushTable(table);                                 // self
     lua.PushParameters(parameters);                      // parameters
@@ -63,7 +71,8 @@ public static class LuaStateExtension {
     table.UpdateAllGoInTable();
   }
 
-  public static LuaTable CallFunction(this LuaState lua, LuaTable table, string name, object[] parameters) {
+  public static LuaTable CallFunction(this LuaState lua, LuaTable table, string name, object[] parameters)
+  {
     lua.PushFunctionFromTable(table, name);
     lua.PushTable(table);                                     // self
     lua.PushParameters(parameters);                           // parameters
@@ -76,7 +85,8 @@ public static class LuaStateExtension {
     return ret;
   }
 
-  public static LuaTable CallFunction(this LuaState lua, int index, object[] parameters) {
+  public static LuaTable CallFunction(this LuaState lua, int index, object[] parameters)
+  {
     lua.RawGetI(LuaDef.LUA_REGISTRYINDEX, index);
     lua.PushParameters(parameters);
     var status = lua.PCall(parameters.Length, 1, 0);      // call
@@ -87,35 +97,43 @@ public static class LuaStateExtension {
     return ret;
   }
 
-  public static void PushParameters(this LuaState lua, object[] parameters) {
-    foreach (var value in parameters) {
-      if (value == null) {
+  public static void PushParameters(this LuaState lua, object[] parameters)
+  {
+    foreach (var value in parameters)
+    {
+      if (value == null)
+      {
         lua.PushNil();
         continue;
       }
 
-      if (value is LuaTable) {
-        lua.PushTable((LuaTable) value);
+      if (value is LuaTable)
+      {
+        lua.PushTable((LuaTable)value);
         continue;
       }
 
-      if (value is float || value is double) {
-        lua.PushNumber((double) value);
+      if (value is float || value is double)
+      {
+        lua.PushNumber((double)value);
         continue;
       }
 
-      if (value is int || value is long || value is short || value is byte) {
-        lua.PushNumber((double) value);
+      if (value is int || value is long || value is short || value is byte)
+      {
+        lua.PushNumber((double)value);
         continue;
       }
 
-      if (value is ulong || value is uint || value is ushort) {
-        lua.PushUInt64((ulong) value);
+      if (value is ulong || value is uint || value is ushort)
+      {
+        lua.PushUInt64((ulong)value);
         continue;
       }
 
-      if (value is string) {
-        lua.PushString((string) value);
+      if (value is string)
+      {
+        lua.PushString((string)value);
         continue;
       }
     }
